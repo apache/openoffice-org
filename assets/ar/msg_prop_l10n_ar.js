1c8af4d9725b7ea299617ff19c4f3bbf33aeaa79
Dave Fisher <dave@davefisher.tech>
2020-11-02 12:57:15 -0800

    Migration of ar assets

---
+++ assets/ar/msg_prop_l10n_ar.js
@@ -0,0 +1,23 @@
+/* This file is to maintain text information that can be used on the download webpage via JavaScript when
+ * including this file.
+ *
+ * Instead of hard coded text in the green box please use these variables and translate it to your favorite language.
+ *
+ * Wrong:   "Download Apache OpenOffice"
+ * Correct: l10n.index_redirect_text
+ */
+
+
+
+// Object that contains all following variables.
+var l10n = new Object();
+
+// The following variables are used in the "index.html" file:
+
+l10n.index_alert_box_show			= true;
+l10n.index_alert_box_background_color		= "#F5A9A9";		// More see "http://html-color-codes.info/"
+l10n.index_alert_box_href			= "https://openoffice.apache.org/translate.html";		// Make the whole alert box clickable with a link.
+									// Maximum 49 characters.
+l10n.index_alert_box_headline_text		= "We need your help to complete translation into Arabic!";
+									// Maximum 630 characters.
+l10n.index_alert_box_text_text			= "This note is in English because we have no one to translate it into your language. The links on this page will help you download and install an older version of Apache OpenOffice.<br /> It is missing many bug fixes, performance enhancements and even new features that are in Apache OpenOffice 4.1.7.<br />We would be happy to make Apache OpenOffice available in Arabic, but we need help completing the translation of the user interface.<br /><br /> If you are interested in helping translate, please click this text to review the information. You can send a note to our mailing list \"L10N@openoffice.apache.org\" if you want to volunteer or if you have questions.";
