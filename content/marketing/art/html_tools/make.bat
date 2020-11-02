b5227cbece425c199c40ac14e034a7d166658bc1
Dave Fisher <dave@davefisher.tech>
2020-11-02 11:48:02 -0800

    git-site-role commit from copy_staging.sh

---
+++ content/marketing/art/html_tools/make.bat
@@ -0,0 +1,11 @@
+copy werk_in_uitvoering.png ..\
+
+copy /B pre.txt+index.txt+post.txt ..\index.html
+copy /B pre.txt+about-project.txt+post.txt ..\about-project.html
+copy /B pre.txt+about-issuezilla.txt+post.txt ..\about-issuezilla.html
+copy /B pre.txt+about-issuezilla-uploading_files.txt+post.txt ..\about-issuezilla-uploading_files.html
+copy /B pre.txt+about-legal.txt+post.txt ..\about-legal.html
+copy /B pre.txt+about-participation.txt+post.txt ..\about-participation.html
+copy /B pre.txt+about-request-graphics.txt+post.txt ..\about-request-graphics.html
+copy /B pre.txt+to-do-artwork.txt+post.txt ..\to-do-artwork.html
+copy /B pre.txt+to-do-maintenance.txt+post.txt ..\to-do-maintenance.html
