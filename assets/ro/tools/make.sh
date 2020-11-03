dc822c3d052485a73a19f827fc04aa46a9ca369d
Dave Fisher <dave@davefisher.tech>
2020-11-03 07:33:10 -0800

    Migration of ro assets

---
+++ assets/ro/tools/make.sh
@@ -0,0 +1,25 @@
+cp werk_in_uitvoering.png ../
+
+less  pre.txt index.txt post.txt > ../index.html
+less  pre.txt about_project.txt post.txt > ../about-project.html
+less pre.txt about-downloads.txt post.txt > ../about-downloads.html
+less pre.txt about-spellcheck.txt post.txt > ../about-spellcheck.html
+less pre.txt doc_faq_index.txt post.txt > ../faq.html
+less pre.txt iz-tutorial.txt post.txt > ../iz-tutorial.html
+less pre.txt background.txt post.txt > ../background.html
+less pre.txt adresses.txt post.txt > ../adresses.html
+less pre.txt about-mailinglist.txt post.txt > ../about-mailinglist.html
+less pre.txt credits.txt post.txt > ../credits.html
+less pre.txt about-legal.txt post.txt > ../about-legal.html
+less pre.txt about-helping.txt post.txt > ../about-helping.html
+less pre.txt about-issuezilla.txt post.txt > ../about-issuezilla.html
+less pre.txt about-documentation.txt post.txt > ../about-documentation.html
+less pre.txt about-documentation.txt post_lev1.txt > ../doc/index.html
+less pre.txt about-product.txt post.txt > ../about-product.html
+less pre.txt about-cdrom.txt post.txt > ../about-cdrom.html
+less pre.txt targets.txt post.txt > ../targets.html
+less pre.txt contr_spellch.txt post.txt > ../contr_spellch.html
+less pre.txt localization.txt post.txt > ../localization.html
+less pre.txt langtools.txt post.txt > ../langtools.html
+less pre.txt press.txt post.txt > ../press.html
+less pre.txt to-do.txt post.txt > ../to-do.html
