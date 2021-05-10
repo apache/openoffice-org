5e274ad1498f59f3d5245df77dbad12b4f4bde9d
mseidel <mseidel@apache.org>
2021-05-10 21:38:31 +0200

    Change to text

--- assets/ta/msg_prop_l10n_ta.js
+++ assets/ta/msg_prop_l10n_ta.js
@@ -14,10 +14,10 @@ var l10n = new Object();
 
 // The following variables are used in the "index.html" file:
 
-l10n.index_alert_box_show					= true;
+l10n.index_alert_box_show			= true;
 l10n.index_alert_box_background_color		= "#F5A9A9";		// More see "http://html-color-codes.info/"
-l10n.index_alert_box_href					= "https://openoffice.apache.org/translate.html";		// Make the whole alert box clickable with a link.
+l10n.index_alert_box_href			= "https://openoffice.apache.org/translate.html";		// Make the whole alert box clickable with a link.
 									// Maximum 49 characters.
-l10n.index_alert_box_headline_text			= "We need your help to complete translation into Tamil!";
+l10n.index_alert_box_headline_text		= "We need your help to complete translation into Tamil!";
 									// Maximum 630 characters.
-l10n.index_alert_box_text_text				= "This note is in English because we have no one to translate it into your language. The links on this page will help you download and install all other available versions of Apache OpenOffice.<br />We would be happy to make Apache OpenOffice available in Tamil, but we need help completing the translation of the user interface.<br /><br /> If you are interested in helping translate, please click this text to review the information. You can send a note to our mailing list \"L10N@openoffice.apache.org\" if you want to volunteer or if you have questions.";
+l10n.index_alert_box_text_text			= "This note is in English because we have no one to translate this webpage and others into your language. The links on the page will help you download and install Apache OpenOffice with Tamil translation. But we still need help with this website. <br /><br /> If you are interested in helping translate, please click this text to review the information. You can send a note to our mailing list \"L10N@openoffice.apache.org\" if you want to volunteer or if you have questions.";
