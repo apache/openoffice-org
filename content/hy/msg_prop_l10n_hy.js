3fd933cabd1724a723ad15092bc34e2dceddb645
jenkins <builds@apache.org>
2021-05-13 17:22:20 +0000

    git-site-role commit from build_staging.sh

--- content/hy/msg_prop_l10n_hy.js
+++ content/hy/msg_prop_l10n_hy.js
@@ -14,10 +14,10 @@ var l10n = new Object();
 
 // The following variables are used in the "index.html" file:
 
-l10n.index_alert_box_show			= true;
-l10n.index_alert_box_background_color		= "#F5A9A9";		// More see "http://html-color-codes.info/"
-l10n.index_alert_box_href			= "https://openoffice.apache.org/translate.html";		// Make the whole alert box clickable with a link.
-									// Maximum 49 characters.
+l10n.index_alert_box_show				= false;
+l10n.index_alert_box_background_color	= "#F5A9A9";		// More see "http://html-color-codes.info/"
+l10n.index_alert_box_href				= "https://openoffice.apache.org/translate.html";		// Make the whole alert box clickable with a link.
+										// Maximum 49 characters.
 l10n.index_alert_box_headline_text		= "We need your help to complete translation into Armenian!";
-									// Maximum 630 characters.
+										// Maximum 630 characters.
 l10n.index_alert_box_text_text			= "This note is in English because we have no one to translate it into your language.<br /><br />We would be happy to make Apache OpenOffice available in Armenian, but we need help completing the translation of the user interface.<br /><br /> If you are interested in helping translate, please click this text to review the information. You can send a note to our mailing list \"L10N@openoffice.apache.org\" if you want to volunteer or if you have questions.";
