6cf269bdec2036ce2ba97a7e0f7598af276b55bc
jenkins <builds@apache.org>
2022-04-14 13:20:51 +0000

    git-site-role commit from build_staging.sh

--- content/content/id/ie6.js
+++
@@ -1,8 +0,0 @@
-var browser=navigator.appName;
- var b_version=navigator.appVersion;
- var version=parseFloat(b_version);
- if ((browser=="Microsoft Internet Explorer") && (version=6))
- {
-   document.write('  <link rel="stylesheet" href="ie6.css" type="text/css" />');
-   alert("Your browser cannot display this site correctly - please ask your vendor for an upgrade");
-}
