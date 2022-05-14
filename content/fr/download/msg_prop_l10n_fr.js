294697b624230de8e0f819be006ed1feb096aa95
jenkins <builds@apache.org>
2022-05-14 15:40:46 +0000

    git-site-role commit from copy_staging.sh

--- content/fr/download/msg_prop_l10n_fr.js
+++ content/fr/download/msg_prop_l10n_fr.js
@@ -359,6 +359,7 @@ l10n.dl_analyze_table_javascript_text		= "JavaScript&nbsp;functions/variables";
 
 
 // General: Links to webpage files that could be localized.
+l10n.dl_rel_notes_aoo4112_link			= "https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=211884130";
 l10n.dl_rel_notes_aoo4111_link			= "https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=191334515";
 l10n.dl_rel_notes_aoo4110_link			= "https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=181305702";
 l10n.dl_rel_notes_aoo419_link			= "https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=173086426";
