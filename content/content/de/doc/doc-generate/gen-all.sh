7986fab87b10802827c2f0baad429d8f05f5bf04
jenkins <builds@apache.org>
2023-03-03 14:59:42 +0000

    git-site-role commit from build_staging.sh

--- content/content/de/doc/doc-generate/gen-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Generating JS-Version..."
-./doc-generate.pl index.tt > ../index.html
-
-echo "Generating Non-JS-Version"
-./doc-generate.pl index-nojs.tt > ../index-nojs.html
-
