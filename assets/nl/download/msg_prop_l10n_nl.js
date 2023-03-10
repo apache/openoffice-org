044ffc922cdb6afa49cd9a6051f11a2bb5d51ee5
mseidel <mseidel@apache.org>
2023-03-10 18:17:51 +0100

    Add link to Dutch Release Notes

--- assets/nl/download/msg_prop_l10n_nl.js
+++ assets/nl/download/msg_prop_l10n_nl.js
@@ -353,6 +353,7 @@ l10n.dl_analyze_table_javascript_text		= "JavaScript&nbsp;functies/variabelen";
 
 
 // General: Links to webpage files that could be localized.
+l10n.dl_rel_notes_aoo4114_link			= "https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=240885214";
 l10n.dl_rel_notes_aoo4113_link			= "https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=217391564";
 l10n.dl_rel_notes_aoo4112_link			= "https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=211882541";
 l10n.dl_rel_notes_aoo4111_link			= "https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=186878207";
