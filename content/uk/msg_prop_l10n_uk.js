c2c3324778caa6c59f774914764f44562d1d3c3e
jenkins <builds@apache.org>
2023-10-17 16:01:41 +0000

    git-site-role commit from build_staging.sh

--- content/uk/msg_prop_l10n_uk.js
+++ content/uk/msg_prop_l10n_uk.js
@@ -14,10 +14,10 @@ var l10n = new Object();
 
 // The following variables are used in the "index.html" file:
 
-l10n.index_alert_box_show			= true;
-l10n.index_alert_box_background_color		= "#F5A9A9";		// More see "http://html-color-codes.info/"
-l10n.index_alert_box_href			= "https://openoffice.apache.org/translate.html";		// Make the whole alert box clickable with a link.
-									// Maximum 49 characters.
+l10n.index_alert_box_show				= true;
+l10n.index_alert_box_background_color	= "#F5A9A9"; // More see "http://html-color-codes.info/"
+l10n.index_alert_box_href				= "https://openoffice.apache.org/translate.html"; // Make the whole alert box clickable with a link.
+										// Maximum 49 characters.
 l10n.index_alert_box_headline_text		= "We need your help to complete translation into Ukrainian!";
-									// Maximum 630 characters.
+										// Maximum 630 characters.
 l10n.index_alert_box_text_text			= "This note is in English because we need your help to translate Apache OpenOffice and this website into your language. The links on this page will help you download and install Apache OpenOffice in all currently available languages. <br /><br /> If you are interested in helping translate, please click this text to review the information. You can send a note to our mailing list \"L10N@openoffice.apache.org\" if you want to volunteer or if you have questions.";
