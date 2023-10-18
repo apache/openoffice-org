37b45d47c6881ebbc24bc216d3d9336abec2de71
Олексій <john.silver.heart@gmail.com>
2023-10-18 13:55:44 +0300

    Update msg_prop_l10n_uk.js to Ukrainian
    Complete translating msg_prop_l10n_uk.js to Ukrainian

--- assets/uk/msg_prop_l10n_uk.js
+++ assets/uk/msg_prop_l10n_uk.js
@@ -1,23 +1,22 @@
-/* This file is to maintain text information that can be used on the download webpage via JavaScript when
- * including this file.
- *
- * Instead of hard coded text in the green box please use these variables and translate it to your favorite language.
- *
- * Wrong:   "Download Apache OpenOffice"
- * Correct: l10n.index_redirect_text
- */
+/* Цей файл призначений для збереження текстової інформації, яку можна використовувати на веб-сторінці завантаження за допомогою JavaScript
+* включаючи цей файл.
+*
+* Замість жорстко закодованого тексту в зеленому полі, будь ласка, використовуйте ці змінні та перекладіть його на вашу улюблену мову.
+*
+* Неправильно: "Завантажити Apache OpenOffice"
+* Правильно: l10n.index_redirect_text
+*/
 
 
-
-// Object that contains all following variables.
+// Об'єкт, який містить усі наступні змінні.
 var l10n = new Object();
 
-// The following variables are used in the "index.html" file:
+// Наступні змінні використовуються у файлі "index.html":
 
 l10n.index_alert_box_show				= true;
-l10n.index_alert_box_background_color	= "#F5A9A9"; // More see "http://html-color-codes.info/"
-l10n.index_alert_box_href				= "https://openoffice.apache.org/translate.html"; // Make the whole alert box clickable with a link.
-										// Maximum 49 characters.
-l10n.index_alert_box_headline_text		= "We need your help to complete translation into Ukrainian!";
-										// Maximum 630 characters.
+l10n.index_alert_box_background_color	= "#F5A9A9"; // Див більше у "http://html-color-codes.info/"
+l10n.index_alert_box_href				= "https://openoffice.apache.org/translate.html"; // Зробити усе вікно сповіщення доступним для натискання за допомогою посилання.
+										// Макс 49 символів.
+l10n.index_alert_box_headline_text		= "Нам потрібна ваша допомога для завершення перекладу українською!";
+										// Макс 630 символів.
 l10n.index_alert_box_text_text			= "This note is in English because we need your help to translate Apache OpenOffice and this website into your language. The links on this page will help you download and install Apache OpenOffice in all currently available languages. <br /><br /> If you are interested in helping translate, please click this text to review the information. You can send a note to our mailing list \"L10N@openoffice.apache.org\" if you want to volunteer or if you have questions.";
